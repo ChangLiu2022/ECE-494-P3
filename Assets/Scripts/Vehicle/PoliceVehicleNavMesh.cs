2f891d22b0d30c5d4d2f2a31505a4ea73d8ef274
ChangLiu <98478845+ChangLiu2022@users.noreply.github.com>
2026-03-22 21:57:12 -0400

    better popo car

---
+++ Assets/Scripts/Vehicle/PoliceVehicleNavMesh.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PoliceVehicleNavMesh : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    private Transform _target;
+    private bool _chasing;
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void OnEnable()
+    {
+        EventBus.Subscribe<GameEvents.VehicleEnterEvent>(OnVehicleEnter);
+        EventBus.Subscribe<GameEvents.VehicleExitEvent>(OnVehicleExit);
+    }
+
+    private void OnDisable()
+    {
+        EventBus.Unsubscribe<GameEvents.VehicleEnterEvent>(OnVehicleEnter);
+        EventBus.Unsubscribe<GameEvents.VehicleExitEvent>(OnVehicleExit);
+    }
+
+    private void OnVehicleEnter(GameEvents.VehicleEnterEvent evt)
+    {
+        _target = evt.vehicleTransform;
+        _chasing = true;
+        _agent.isStopped = false;
+    }
+
+    private void OnVehicleExit(GameEvents.VehicleExitEvent evt)
+    {
+        _chasing = false;
+        _target = null;
+        _agent.isStopped = true;
+    }
+
+    private void Update()
+    {
+        if (_chasing && _target != null)
+            _agent.SetDestination(_target.position);
+    }
+}
