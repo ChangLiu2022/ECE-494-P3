59fdaacabcea03b66f2207f88452915fc90ff885
moemente <56563422+moemente@users.noreply.github.com>
2026-03-29 21:40:01 -0400

    broken

---
+++ Assets/ExitDoorScript.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using static GameEvents;
+
+public class ExitDoorScript : MonoBehaviour
+{
+    [SerializeField] private Transform teleport_position;
+    [SerializeField] public float interactRange = 1.5f;
+    [SerializeField] private CameraFollow cam;
+
+    private bool can_leave = false;
+    private Transform player;
+
+    private void OnEnable()
+    {
+        EventBus.Subscribe<GoldEvent>(OnGoldCollected);
+    }
+
+    private void OnDisable()
+    {
+        EventBus.Unsubscribe<GoldEvent>(OnGoldCollected);
+    }
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Body").transform;
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.position);
+        if (distance <= interactRange && Input.GetKeyDown(KeyCode.E))
+        {
+            if (can_leave)
+            {
+                player.position = teleport_position.position;
+                cam.SetTarget(player);
+            }
+
+            else
+                Debug.Log("Door locked — gold not collected yet.");
+        }
+    }
+
+    private void OnGoldCollected(GoldEvent e)
+    {
+        can_leave = true;
+    }
+}
