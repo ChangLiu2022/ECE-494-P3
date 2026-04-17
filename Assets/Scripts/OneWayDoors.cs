7647980b6c89bee08667c4797914d59552706562
moemente <56563422+moemente@users.noreply.github.com>
2026-04-17 18:07:22 -0400

    actually fixed the fucking door issue

--- Assets/Scripts/OneWayDoors.cs
+++ Assets/Scripts/OneWayDoors.cs
@@ -1,26 +1,10 @@
 using UnityEngine;
 
+
 public class OneWayDoors : MonoBehaviour
 {
-    private BoxCollider zone;
-    private Transform player;
-    private bool hasFired = false;
-
-    private void Start()
+    private void OnTriggerExit(Collider other)
     {
-        zone = GetComponent<BoxCollider>();
-        GameObject body = GameObject.FindWithTag("Player");
-        if (body != null) player = body.transform;
-    }
-
-    private void Update()
-    {
-        if (hasFired || player == null) return;
-
-        if (zone.bounds.Contains(player.position))
-        {
-            hasFired = true;
-            EventBus.Publish(new GameEvents.PlayerEnteredMapEvent());
-        }
+        EventBus.Publish(new GameEvents.PlayerEnteredMapEvent());
     }
 }
